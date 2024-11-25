8975230c4975f0a3955cff8beac0a2c5f40c5ae6
unknown <nanda.ahmad2007@gmail.com>
2024-11-25 21:45:44 +0700

    Server side update

--- server/main.py
+++ server/main.py
@@ -1,8 +1,41 @@
 import asyncio
 import websockets
 from gpiozero import LED
+import json
 
+# Response status
+class status:
+    OK = 'OK'
+    ERROR = 'ERROR'
+    
+# Order options
+class order:
+    coffee = 'coffee'
+    juice = 'juice'
+    
+# Message types
+class msgtype:
+    order = 'order'
+    done = 'done'
+    error = 'error'
+    
+# Dispensing duration (in second(s))
+motor_duration = 5
+
+# Status LED
 led = LED(26)
+
+# Connection to motors' relays
+# coffee, juice
+motors = [ LED(99), LED(99) ] # TO DO
+
+async def process_order(option):
+    global motors, motor_duration
+    selected_motor = motors[0] if option == order.coffee else motors[1]
+    
+    selected_motor.on() # turn on the motor
+    await asyncio.sleep(motor_duration) # wait until dispenser finish dispensing the drink
+    selected_motor.off() # turn off the motor
 
 # Define a handler for WebSocket connections
 async def handle_connection(websocket):
@@ -10,17 +43,19 @@ async def handle_connection(websocket):
     try:
         async for message in websocket:
             print(f"Received: {message}")
-            if message == "ledon":
-                print("ledon")
-                led.on()
-            elif message == "ledoff":
-                print("ledoff")
-                led.off()
-            elif message == "ping":
-                pong_waiter = await websocket.ping()
-                await websocket.send(await pong_waiter)
-            response = f"Echo: {message}"
-            await websocket.send(response)
+            data = json.loads(message)
+            
+            response = {}
+            
+            match message['type']:
+                case 'order':
+                    # Process the incoming order
+                    await process_order(message['option'])
+                    await websocket.send(json.dumps({
+                        'status': status.OK,
+                        'type': msgtype.done,
+                    }))
+            
     except websockets.exceptions.ConnectionClosed as e:
         print(f"Connection closed: {e}")
 
